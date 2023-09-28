031a8d9a741976ddaed4a699f3d5f8f0d3e3f381
Islomjon <ergashevislomjon1020@gmail.com>
2023-09-28 11:42:44 +0500

    fixed: test response

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require('express');
+const express = require("express");
 require("dotenv").config();
-const appRoute = require('./routes/route.js')
+const appRoute = require("./routes/route.js");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Working!");
+});
+
 /** routes */
-app.use('/api', appRoute);
+app.use("/api", appRoute);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-})
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
