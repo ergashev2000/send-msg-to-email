0288be959771d6311bf66930c7e048278d383046
Islomjon <ergashevislomjon1020@gmail.com>
2024-02-16 17:40:52 +0500

    fix

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 require("dotenv").config();
+
+const cors = require("cors");
+
 const appRoute = require("./routes/route.js");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
